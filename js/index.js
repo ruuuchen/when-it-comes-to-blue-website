389c75e6c813cd9f4f134ae4c844d67cbf6efe30
Ru Chen <wrchen0516@gmail.com>
2018-07-26 13:04:10 +0800

    updata map

--- js/index.js
+++ js/index.js
@@ -17,3 +17,16 @@ $("button.btn").click(function(){
     $("button.btn").removeClass("btn-success");
   },1500);
 });
+
+// 地圖-----------------------
+var map;
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 25.013192, lng: 121.541061},
+    zoom: 14,
+  });
+  var marker = new google.maps.Marker({
+    position: {lat: 25.013192, lng: 121.541061},
+    map: map
+  });
+}
