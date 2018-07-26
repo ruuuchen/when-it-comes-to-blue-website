48a4687dca970939ffecd61e80657ee1ae6f5ee9
Ru Chen <wrchen0516@gmail.com>
2018-07-26 11:35:22 +0800

    update skrollr

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-// skrollr初始值----------------------- 
+// skrollr初始值-----------------------
 var s = skrollr.init();
+// 取消 mobile 上的 skrollr 效果
+$(function () {
+  // initialize skrollr if the window width is large enough
+  if ($(window).width() > 768) {
+    skrollr.init();
+  }
 
-// 訂閱按鈕----------------------- 
+  // disable skrollr if the window is resized below 768px wide
+  $(window).on('resize', function () {
+    if ($(window).width() <= 768) {
+      skrollr.init().destroy(); // skrollr.init() returns the singleton created above
+    }
+  });
+});
+
+// 訂閱按鈕-----------------------
 $("button.btn").click(function(){
   $("button.btn").text("成功"+" :)");
   $("button.btn").addClass("btn-success");
   $("input").val("");
-  
+
   setTimeout(function(){
     $("button.btn").text("✓ "+"訂閱");
     $("button.btn").removeClass("btn-success");
